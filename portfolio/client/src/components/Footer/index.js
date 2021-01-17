3cea4701bcc29c6e7dfbf3a75eecf1a967a76d1b
Brandon Piercy <b.piercy567@gmail.com>
2021-01-17 09:34:04 -0800

    changed the hover animation from fade to invert because it matches the website's pallette better

---
+++ portfolio/client/src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./style.css";
+
+Footer = () => {
+    return (
+        <div className="footer">
+            
+        </div>
+    )
+}
