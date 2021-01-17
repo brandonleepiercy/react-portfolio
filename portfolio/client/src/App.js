83a6afa135faf58197c9f7f60df46dea84641ada
Brandon Piercy <b.piercy567@gmail.com>
2021-01-16 22:25:25 -0800

    created pages, updated the app.js file to return those pages

--- portfolio/client/src/App.js
+++ portfolio/client/src/App.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+import Contact from "./pages/Contact";
+import NoMatch from "./pages/NoMatch";
+import Nav from "./components/Navbar";
 
 function App() {
   return (
-    <div class="container">
-      Hello World!
-    </div>
+    <Router>
+      <div>
+        <Nav />
+        <Switch>
+            <Route exact path="/" component ={Home} />
+            <Route exact path ="/about" component={About} />
+            <Route exact path = "/projects" component={Projects} />
+            <Route exact path = "/contact" component={Contact} />
+            <Route component={NoMatch} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
