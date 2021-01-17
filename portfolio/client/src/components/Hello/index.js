e038b3a0797798b5e6df6d28d662ab3ffbb18f26
Brandon Piercy <b.piercy567@gmail.com>
2021-01-17 15:20:15 -0800

    perfected mobile responsiveness with the way the text flows in the home 'hello'

--- portfolio/client/src/components/Hello/index.js
+++ portfolio/client/src/components/Hello/index.js
@@ -3,16 +3,23 @@ import './style.css';
 
 function Hello() {
     return(
-        <div className="row">
+        <div>
             <div className="invis-box">
                 <div className="green-box">
-                    <div className="hi-group">
-                        <h1>Hi, I'm Brandon.</h1>
-                        <h1>Hi, I'm Brandon.</h1>
-                        <h1>Hi, I'm Brandon.</h1>
-                    </div>
-                    <div className="hi-text">
-                        <h2>I am a full-stack web developer. I specialize in Javascript, Node, Express, MySQL, MongoDB and React.js</h2>
+                    <div className="text-group">
+                        <div className="hi-group">
+                            <h1 className="brandon-text">
+                                Hi, I'm Brandon.
+                                <br />
+                                Hi, I'm Brandon.
+                                <br />
+                                Hi, I'm Brandon
+                                <br />
+                            </h1>
+                        </div>
+                        <div className="hi-text">
+                            <h2 className="description-text">I am a full-stack web developer. I specialize in Javascript, Node, Express, MySQL, MongoDB and React.js</h2>
+                        </div>
                     </div>
                 </div>
             </div>
