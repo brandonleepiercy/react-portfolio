3a4813e353925c6bb1965466f737934c13e23b41
Brandon Piercy <b.piercy567@gmail.com>
2021-02-02 14:07:48 -0800

    url edit

--- client/src/components/ProjectGroup/index.js
+++ client/src/components/ProjectGroup/index.js
@@ -14,7 +14,7 @@ function ProjectGroup() {
                     <h1>Snippet</h1>
                     <p>Snippet is a full-stack React.js application with a MySQL database, routed with Express.js. The application stores snippets of code that developers may want to reference for future use. Users can sign up for an account, but they do not have to in order to search and browse through the snippets already in the database. Users do have to create an account to create code snippets and insert them into the database. When creating code, the user assigns a title, description and tags to make it easier for other users to search for and find. Commenting, likes, copies and other user data is tracked. The application was created by a group of 5 people, myself included. Development started January 11th of 2021 and continues for the forseeable future. My primary responsibilities in this project were creating API end points for our sequelize models, and creating the front-end and logic for the "create-code" page.</p>
                     <div className="project-link-box">
-                        <a target="_blank" rel="noreferrer" href="https://www.snippet.ltd">Heroku Deployment</a>
+                        <a target="_blank" rel="noreferrer" href="http://www.snippet.ltd">Heroku Deployment</a>
                         <br />
                         <a target="_blank" rel="noreferrer" href="https://github.com/ma-wong/project-3/">Github Repo</a>
                     </div>
