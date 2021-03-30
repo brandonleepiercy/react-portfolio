be98d59cc39dd40d2b417f973e69886634d813c7
brandonleepiercy <b.piercy567@gmail.com>
2021-03-30 11:54:02 -0700

    changed the about section

--- client/src/components/InfoBlock/index.js
+++ client/src/components/InfoBlock/index.js
@@ -12,18 +12,19 @@ function InfoBlock() {
             </div>
             <div className = "box box-2">
                 <h1>About Me:</h1>
-                <p>I began learning how to develop software in October of 2020 through UCLA, and in 4 challenging months, I have gone from knowing nothing about software development to being able to develop and maintain a full-stack web application on my own. It was not easy by any means, but seemingly insurmountable challenges are one of the few things I find rewarding. Aside from software, in 2020 I graduated from UCSB with a B.A. in Communication, and I am a self-taught mechanic. For the past two years I've been fixing and upgrading my 2013 VW CC in my parking spot, and I track it at Willow Springs/Buttonwillow when I have the time.</p>
+                <p>I began learning how to develop software in October of 2020 through UCLA, and in 4 challenging months, I have gone from knowing nothing about software development to being able to develop and maintain a full-stack web application on my own. It was a difficult process, but I enjoy challenges and that made the journey a lot more rewarding. Aside from software, in 2020 I graduated with a B.A. in Communication from UCSB, where I also completed courses in Economics, Accounting and Technology Management for my own benefit. On the weekends, for the past two years I've been fixing and modifying my car in my parking spot. I attend track events with my car whenever I can, and I'm hoping to be licensed by the SCCA for GTA and GRID racing soon.</p>
                 <h1>Skills:</h1>
                 <ul>
                     <li>React.js</li>
-                    <li>Node.js, Express.js and Express-Handlebars</li>
+                    <li>Node.js and Express.js</li>
                     <li>MySQL and Sequelize</li>
                     <li>MongoDB, Mongoose and Atlas</li>
                     <li>HTML, CSS and Bootstrap</li>
-                    <li>CSS Flex and Grids</li>
+                    <li>CSS FlexBoxes and Grids</li>
                     <li>Javascript and JQuery</li>
-                    <li>API Interaction/ AJAX and Axios</li>
+                    <li>API construction (REST and SOAP)</li>
                     <li>Git Flow, Branch Management</li>
+                    <li>Data Structures and Algorithms</li>
                     <li>App Deployment</li>
                     <li>Performance Optimization/ Repo Minimization</li>
                     <li>SEO</li>
